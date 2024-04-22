fd71927b25cc8660f77f69ac7cc4a0ddb36d92fa
Pandya <104791367+KishanPandya405@users.noreply.github.com>
2024-04-22 14:08:22 +0000

    datatypes

---
+++ .github/hari/variable.js
@@ -0,0 +1,15 @@
+const accountId = 846059
+let accountEmail = "kishanpandya405@gmail.com"
+let accountPassword = "846059503"
+let accountCity = "Gandhinagar"
+
+// accountId= 2 //not allocated
+
+accountEmail ="tridevdigital11@gmail.com"
+accountPassword = "tridev@1234"
+accountCity = "Ahmedabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* Prefer not to use var
+   because of issue in block scope and functional scope  */
