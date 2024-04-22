0b9e8ec973337b5fe6a17ec97a7a4fcd91daae12
Pandya <104791367+KishanPandya405@users.noreply.github.com>
2024-04-22 14:02:35 +0000

    container commit done

---
+++ .github/hari/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer verison
+// alert (3 + 3) // we are using nodejs not browser
+console.log(3 
+    +
+     3) // code redebelity should be high
+     //console.log ("kishan")
+
+     let name = "kishan"
+     let age = 26
+     let isLoggedIn = false
+
+      // Primitive data types
+     // number => 2 to power 53
+     // bigint
+     // sting => ""
+     // boolean => true/false
+     // null => standalone value                       
+     // undifined => value of not assign
+     // symble => unique
+
+     // object =>
+
+     console.log(typeof undifined); // type undifined
+     console.log(typeof null); // type object
