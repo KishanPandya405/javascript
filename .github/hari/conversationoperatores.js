f3524077e89d87458b8b40d765bd2ba4ce7f9099
Pandya <104791367+KishanPandya405@users.noreply.github.com>
2024-04-24 19:38:00 +0000

    conversationoperatores.js

---
+++ .github/hari/conversationoperatores.js
@@ -0,0 +1,27 @@
+let score = "32abc"
+
+//console.log (typeof score);
+//console.log (typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; false => 0 ;
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => True ; 0 => false
+// "" => False
+// "Kishan" => True
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
